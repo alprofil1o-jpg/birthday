242a3b8edf3c3b38c5cff3363eb5589453892ae0
alprofil1o-jpg <alprofil1o@gmail.com>
2026-04-02 11:04:07 +0200

    a

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import BirthdayTracker from './pages/BirthdayTracker';
-import './index.css';
-
-function App() {
-  return <BirthdayTracker />;
-}
-
-export default App;
