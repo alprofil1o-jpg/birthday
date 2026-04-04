e705aec6c54a841a70dba57182156031ef461cd9
alprofil1o-jpg <alprofil1o@gmail.com>
2026-04-04 17:00:30 +0200

    g

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import BirthdayTracker from './pages/BirthdayTracker';
+import { autoReRegisterPush } from './lib/pushHelper';
 import './index.css';
 
 function App() {
+  useEffect(() => {
+    // Every page load: silently re-register push subscription
+    // This fixes the issue where server restarts lose all subscriptions
+    autoReRegisterPush();
+  }, []);
+
   return <BirthdayTracker />;
 }
 
