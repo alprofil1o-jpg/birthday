cb74f93daa781f9f52a8168f1c108fbb76134e9c
alprofil1o-jpg <alprofil1o@gmail.com>
2026-04-08 15:04:32 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -191,3 +191,4 @@ export async function startServer(port) {
 if (import.meta.url === `file://${process.argv[1]}`) {
   startServer(process.env.PORT || 3001);
 }
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
